c420f6cc3bd2c9c1540b5fdc11072b8cc2fd20c6
liufei <liufei@fhkg.com>
2018-04-13 13:21:21 +0800

    tag1.0.1

--- douyin/douyin/ViewController.m
+++ douyin/douyin/ViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
-    self.navigationItem.title = @"ViewController";
+//    self.navigationItem.title = @"ViewController";
 }
 
 
